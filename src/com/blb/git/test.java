6c29fea82f23a1e30583dcdac8c5895452a5be3c
星河 <1315762419@qq.com>
2019-12-23 17:43:46 +0800

    第三次

--- src/com/blb/git/test.java
+++ src/com/blb/git/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("aaaa");
         System.out.println("A-AA-AAA-AAAA-AAAAA");
+        System.out.println("BBBB=BBBBB=CBA");
     }
 }
